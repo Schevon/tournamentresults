dd784e845f9d6be106e9c6ddae6ec6de02555033
Schevon <ojoseph445@gmail.com>
2015-07-28 20:17:35 -0400

    add comments to tournament.sql

--- tournament.sql
+++ tournament.sql
@@ -2,7 +2,10 @@
 --Table definitions for the tournament project.
 
 
---Creating Table name Player
+--Create Table PLAYER
+--PLAYER_ID is used as the primary key to uniquely identify PLAYERS
+--SERIAL is used to autoincrement PLAYER_ID
+
 CREATE TABLE PLAYER(
    PLAYER_ID      SERIAL PRIMARY KEY,
    NAME           TEXT    NOT NULL,
@@ -12,6 +15,12 @@ CREATE TABLE PLAYER(
    NUM_OF_MATCHES INT,
    CREATED_DATE   DATE
  );
+
+--create table MATCH
+--MATCH_ID is used as the primary key to uniquely identify matches
+--SERIAL is used to autoincrement MATCH_ID
+--WINNER_ID is the foreign key in MATCH, it references 
+--PLAYER_ID (the primary key in the PLAYER table).This enforces referential integrity 
 
 CREATE TABLE MATCH(
    MATCH_ID 	  SERIAL PRIMARY KEY,
